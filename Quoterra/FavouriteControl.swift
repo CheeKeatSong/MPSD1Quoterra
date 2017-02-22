3f5968178bf506973a3527a4725343efeb564448
Song Chee Keat <cheekeatsong@gmail.com>
2017-02-22 21:32:00 +0800

    Button Alignment

---
+++ Quoterra/FavouriteControl.swift
@@ -0,0 +1,23 @@
+//
+//  FavouriteControl.swift
+//  Quoterra
+//
+//  Created by Song Chee Keat on 22/02/2017.
+//  Copyright © 2017 Song Chee Keat. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteControl: UIStackView {
+
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder){
+        super.init(coder: coder)
+    }
+    
+    
+}
